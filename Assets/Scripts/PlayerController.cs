b572370688114ddf6a0095aea570f7b1bd90bf74
Cameron Penner <camdpenner@gmail.com>
2015-12-09 11:06:03 -0600

    Simple Animation Demo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	Animator _animator;
+
+	void Start()
+	{
+		_animator = GetComponent<Animator>();
+
+		SetWalking(false);
+	}
+	
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			SetWalking(true);
+
+			transform.Translate(new Vector3(3 * Time.deltaTime, 0, 0));
+		}
+		else
+		{
+			SetWalking(false);
+		}
+	}
+
+	private void SetWalking(bool walking)
+	{
+
+		_animator.SetBool("Walking", walking);
+	}
+}
